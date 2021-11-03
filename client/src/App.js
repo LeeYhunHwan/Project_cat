f4ffef01e34775bb3a4221f6216c6fcab80c68eb
LeeYhunHwan <yhlee1227@naver.com>
2021-11-03 23:31:10 +0900

    sign-in 창 구현중

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
+import './reset.css';
 
 import {
   BrowserRouter as Router,
--- client/src/pages/signIn.js
+++ client/src/pages/signIn.js
@@ -1,8 +1,24 @@
 import axios from "axios";
 import React from 'react';
 
+import {
+    BrowserRouter as Router,
+    Link
+  } from 'react-router-dom';
+
+
+
 export default function SignIn() {
     return (
-        <div>sign in</div>
+        <div className="login-container">
+            <form className="login-form">
+                <label for="login-id">아이디</label>
+                <input id="login-id" type="text" placeholder="아이디를 입력하세요" />
+                <label for="login-password">패스워드</label>
+                <input id="login-password" type="password" placeholder="비밀번호를 입력하세요" />
+                <button type="submit" className="login-container-">로그인하기</button>
+                <Link to="../signUp"><div>회원가입하기</div></Link>
+            </form>
+        </div>
     )
 }
